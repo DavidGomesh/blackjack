1c6fd947507c6b8f2efe2fc5374a64c1b862c15c
David Gomesh <davidgomesh2016@gmail.com>
2025-04-24 17:55:57 -0300

    Initial commit

---
+++ src/Game.tsx
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Game() {
+    return <>
+        <div className='border' style={{width: "50px", height: "100px"}}></div>
+    </>
+}
--- src/test/java/com/gomesh/Blackjack/model/CardTest.java
+++
@@ -1,5 +0,0 @@
-package com.gomesh.Blackjack.model;
-
-public class CardTest {
-    
-}
